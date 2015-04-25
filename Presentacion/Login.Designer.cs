24a43f3e976847233eec895d7246904f7c740bbc
Nicolas Marced <nico@mail.com>
2015-04-25 18:36:41 +0200

    Formulario menu principal

--- Presentacion/Login.Designer.cs
+++ Presentacion/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Presentacion
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.tbUsuario = new System.Windows.Forms.TextBox();
-            this.tbContraseña = new System.Windows.Forms.TextBox();
-            this.bIngresar = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bIngresar = new System.Windows.Forms.Button();
+            this.tbContraseña = new System.Windows.Forms.TextBox();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,63 +44,67 @@
             this.groupBox1.Controls.Add(this.bIngresar);
             this.groupBox1.Controls.Add(this.tbContraseña);
             this.groupBox1.Controls.Add(this.tbUsuario);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.groupBox1.Location = new System.Drawing.Point(12, 74);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(299, 166);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Login";
-            // 
-            // tbUsuario
-            // 
-            this.tbUsuario.Location = new System.Drawing.Point(9, 43);
-            this.tbUsuario.Name = "tbUsuario";
-            this.tbUsuario.Size = new System.Drawing.Size(199, 20);
-            this.tbUsuario.TabIndex = 0;
-            // 
-            // tbContraseña
-            // 
-            this.tbContraseña.Location = new System.Drawing.Point(6, 97);
-            this.tbContraseña.Name = "tbContraseña";
-            this.tbContraseña.Size = new System.Drawing.Size(199, 20);
-            this.tbContraseña.TabIndex = 1;
-            // 
-            // bIngresar
-            // 
-            this.bIngresar.Location = new System.Drawing.Point(218, 137);
-            this.bIngresar.Name = "bIngresar";
-            this.bIngresar.Size = new System.Drawing.Size(75, 23);
-            this.bIngresar.TabIndex = 2;
-            this.bIngresar.Text = "Ingresar";
-            this.bIngresar.UseVisualStyleBackColor = true;
-            this.bIngresar.Click += new System.EventHandler(this.bIngresar_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Usuario:";
+            this.groupBox1.Text = "Iniciar sessión";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(6, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(91, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Contraseña:";
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Usuario:";
+            // 
+            // bIngresar
+            // 
+            this.bIngresar.Location = new System.Drawing.Point(206, 130);
+            this.bIngresar.Name = "bIngresar";
+            this.bIngresar.Size = new System.Drawing.Size(87, 30);
+            this.bIngresar.TabIndex = 2;
+            this.bIngresar.Text = "Ingresar";
+            this.bIngresar.UseVisualStyleBackColor = true;
+            this.bIngresar.Click += new System.EventHandler(this.bIngresar_Click);
+            // 
+            // tbContraseña
+            // 
+            this.tbContraseña.Location = new System.Drawing.Point(6, 97);
+            this.tbContraseña.Name = "tbContraseña";
+            this.tbContraseña.Size = new System.Drawing.Size(199, 21);
+            this.tbContraseña.TabIndex = 1;
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Location = new System.Drawing.Point(9, 43);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(199, 21);
+            this.tbUsuario.TabIndex = 0;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(323, 266);
+            this.ClientSize = new System.Drawing.Size(322, 296);
             this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
